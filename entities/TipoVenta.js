58712c4d929403341a8e9967b13e7c9e38684bee
Nicolas_QuadPoint <quaddyvb@outlook.com>
2021-06-24 22:30:38 -0400

    probando

--- entities/TipoVenta.js
+++ entities/TipoVenta.js
@@ -5,8 +5,10 @@ class TipoVenta extends Entity{
     id_tipo_venta = 0;
     descripcion = 'tipo_venta';
 
-    constructor(){
+    constructor(p_id_tipo_venta=0,p_descripcion='tipo_venta'){
         super();
+        this.id_tipo_venta = p_id_tipo_venta;
+        this.descripcion = p_descripcion;
     }
 
     clone(obj={},safe=false){
